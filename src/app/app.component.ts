6fbd646df8bd124ce62c7040e5928f232ea4f566
patAV98 <kh.ff.love98@gmail.com>
2019-02-25 15:35:14 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LibretaContactosPage } from '../pages/libreta-contactos/libreta-contactos';
+import { AcercaDePage } from '../pages/acerca-de/acerca-de';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +24,9 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Libreta de Contactos', component: LibretaContactosPage},
+      { title: 'Acerca de', component: AcercaDePage}
     ];
 
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import {LibretaContactosPage} from '../pages/libreta-contactos/libreta-contactos';
+import {AcercaDePage} from '../pages/acerca-de/acerca-de';
+import {NuevoContactoPage} from '../pages/nuevo-contacto/nuevo-contacto';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,7 +17,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LibretaContactosPage,
+    AcercaDePage,
+    NuevoContactoPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +30,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LibretaContactosPage,
+    AcercaDePage,
+    NuevoContactoPage
   ],
   providers: [
     StatusBar,
